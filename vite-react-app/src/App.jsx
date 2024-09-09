dcccba541003796a7fd078de12c2f07bbd970b76
Syed Zaheerudeen <t8667262667@gmail.com>
2024-09-09 09:07:08 +0530

    lecture 2 is completed

---
+++ vite-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>hello world of vite with react</h1>
+  )
+}
+
+export default App
