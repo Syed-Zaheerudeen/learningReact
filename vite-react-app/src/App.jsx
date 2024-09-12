38456033e38cdfe3df18a7bc34f60375bc906429
Syed Zaheerudeen <t8667262667@gmail.com>
2024-09-12 22:14:44 +0530

    built counter app to learn hooks

--- vite-react-app/src/App.jsx
+++ vite-react-app/src/App.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+
 
 function App() {
+    let [count,setCount] = useState(0);
+
+    function increment() {
+      if(count < 20) {
+        setCount(count + 1);
+      } else {
+        alert("you cannot go above 20");
+      }
+    }
+
+    function decrement() {
+     if(count > 0) {
+       setCount(count - 1);
+     } else {
+       alert("you cannot go below 0"); 
+     }
+   }
+   
+    
     return (
-    <h1>hello world of vite with react</h1>
+     <>
+ 
+        <div style = {{height: '100vh', backgroundColor: '#000',display: 'flex',flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+          
+        <h1 style = {{color: 'beige'}}>count: {count}</h1>
+          <div style={{display: 'flex', gap: '10px', flexDirection: 'column', width: '155px'}}>
+            <button onClick={increment} style={{background: 'greenyellow', color: 'black', padding: '8px', borderRadius: '8px', cursor: 'pointer', fontWeight: 'bolder'}}>add</button>
+            <button onClick={decrement} style={{background: 'tomato', color: 'black', padding: '8px', borderRadius: '8px', cursor: 'pointer', fontWeight: 'bolder'}}>remove</button>
+          </div>
+        
+         
+        </div>       
+     
+     </>
   )
 }
 
